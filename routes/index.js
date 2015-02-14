fd99e40689c843b08ea1356c836d8f080c04da0a
n <dorman.nathan@gmail.com>
2015-02-14 01:04:31 -1000

    Updated to include basic ratchet layout and process an RSS feed.

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var feed = require("feed-read");
-
-
+var json = {};
+var cAndCFeed = "http://www.calendarwiz.com/calendars/rssfeeder.xml?crd=cityofhonolulu&len=200&days=30&events=100&title=Calendar%20Events&cat=87163";
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  feed("http://www.calendarwiz.com/calendars/rssfeeder.xml?crd=cityofhonolulu&len=200&days=30&events=100&title=Calendar%20Events&cat=87163", function(err, articles){
+  feed(cAndCFeed, function(err, articles){
         if (err)
           throw err;
         articles.forEach(function(entry) {
-          console.log(entry.title);
+          var info = entry.title;
+          var location = info.substring(info.lastIndexOf("@ ") + 2, info.length);
+          console.log(location);
         });
       });
 
   res.render('index', { title: 'Express' });
+});
+router.get('/json', function(req, res, next) {
+  json.feeds = {};
+  json.temp = "test";
+  res.send(json);
 });
 
 module.exports = router;
